f630e4c819e77f7775d39aabe849d42a1bec81de
georgy3000 <71401546+georgy3000@users.noreply.github.com>
2025-06-15 18:30:43 +0700

    Update __init__.py

--- cuda_kernels/__init__.py
+++ cuda_kernels/__init__.py
@@ -1,1 +1,15 @@
+import os
+from pycuda.compiler import SourceModule
+
+def load_kernel(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        source = f.read()
+    return SourceModule(source)
+
+# Компиляция всех ядер
+mod_preprocess = load_kernel("preprocess.cu")
+
+# Доступ к функциям
+filter_low_quality = mod_preprocess.get_function("filter_quality")
 

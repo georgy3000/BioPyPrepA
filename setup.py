56fed19220eccc03c260252b0c1dc94c5c4aa057
georgy3000 <71401546+georgy3000@users.noreply.github.com>
2025-06-16 16:17:20 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BioPyPrepA",  # Имя библиотеки
+    version="0.1.0",     # Первая версия
+    description="GPU-ускоренная библиотека для препроцессинга биологических последовательностей",
+    author="Georgy3000",  # Твоё имя или ник
+    url="https://github.com/georgy3000/BioPyPrepA",
+    packages=find_packages(),  # Автоматически находит все пакеты
+    include_package_data=True,  # включает не-Python файлы, если указан MANIFEST.in
+    install_requires=[
+        "numpy",
+        "pycuda",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
